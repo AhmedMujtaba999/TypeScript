81b9916af7608ba1009df0c035d5b9fea0d42318
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-09 21:08:10 +0530

    use of return type: void, string , number or never

--- funcinTs/myfunction.ts
+++ funcinTs/myfunction.ts
@@ -1,6 +1,7 @@
-function addTwo(num: number ){
+function addTwo(num: number ): number {
   //  num.toUpperCase(); // not allowed as toUpperCase is a method of string type variable not number type variable
     return num+2;
+    //return "hello";
 }
 
 function getUpper(val: string){
@@ -21,7 +22,37 @@ singup("Ahmed", "ahmed@example.com", false); // Output: undefined as the functio
 loginuser("h","h")// if u dont provide the 3 value it is working 
 // cause the ispaid is set false as defualt
 
+// function getVal(myVal: number){
+//     if (myVal> 2){
+//         return true;
+//     }
+//     return "100 ok";
+// }
 
 
+// arrow function syntax
+const getHelo=(s : string ): string => {
+    return "heeee";
+
+}
+
+const heros = ["thor", "spiderman", "ironman"]
+
+//const heros= [1,2,3,4]
+heros.map((hero): string =>{
+    return 'hero is ${hero}'
+    //return 1;
+})
+
+
+function conerror(errmsg: string): void{
+    console.log(errmsg);
+}
+
+
+function hadleerr(Err: string): never{
+    throw new Error(Err);
+
+}
 
 export{}
