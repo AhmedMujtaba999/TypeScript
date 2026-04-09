531b9b47b3275b6b0937387ea01e10afa88dd1a2
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-09 18:51:11 +0530

    when varaibles.ts is combiled with command tsc it will give the pure .js file

---
+++ varaibles.ts
@@ -0,0 +1,16 @@
+let greetings: string = "Ahmed Mujtaba Ali";
+console.log(greetings);
+
+// mynum.touch(); // not allowed as it is a number type variable
+//greetings.toLowercase(); // not allowed toLowerCase is correct method name
+let userId =334455.5; // allowed as number type variable can hold both integer and floating point values
+userId.toFixed(2); // allowed toFixed is a method of number type variable
+
+//userId= "ahmed"; //logically typescript know userid is number so u cant assign string now 
+
+//boolean
+
+let isLoggedIn: boolean= true;
+
+
+export {}
