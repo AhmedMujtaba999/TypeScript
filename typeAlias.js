ac46936807917d9cbc9b5eae29d3a25ef79ff4f1
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-09 19:16:40 +0530

    type alias example : u dont need to write types everytime just writing once and then u can reuse it

---
+++ typeAlias.js
@@ -0,0 +1,30 @@
+"use strict";
+// without type alias
+let user1 = {
+    name: "Ahmed",
+    age: 25
+};
+let user2 = {
+    name: "Ali",
+    age: 27
+};
+let user3 = {
+    name: "Sara",
+    age: 50
+};
+let user4 = {
+    name: "rohit",
+    age: 26
+};
+let user5 = {
+    name: "rohan",
+    age: 9
+};
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+console.log(add(5, 10)); // Output: 15
+console.log(subtract(10, 5)); // Output: 5
+console.log(multiply(5, 10)); // Output: 50
+console.log(divide(10, 5)); // Output: 2
---
+++ typeAlias.ts
@@ -0,0 +1,43 @@
+
+// without type alias
+let user1: {name: string, age: number} = {
+    name: "Ahmed",
+    age: 25
+}
+
+let user2: {name: string, age: number} ={
+    name: "Ali",
+    age: 27
+}
+
+//with type alias
+
+type User ={ name : string, age: number};
+
+let user3: User ={
+    name : "Sara",
+    age: 50
+}
+
+let user4: User={
+    name: "rohit",
+    age: 26
+}
+
+let user5 : User ={
+    name: "rohan",
+    age: 9
+}
+
+// type alias for functions 
+type Add = (a: number, b: number) => number;// pure typescript syntax
+
+let add: Add= (a,b) => a+b;
+let subtract: Add= (a,b)=> a-b;
+let multiply: Add =(a,b)=> a*b;
+let divide: Add = (a,b) => a/b;
+
+console.log(add(5,10)); // Output: 15
+console.log(subtract(10,5)); // Output: 5
+console.log(multiply(5,10)); // Output: 50
+console.log(divide(10,5)); // Output: 2
