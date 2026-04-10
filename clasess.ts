9644314c2da916334d112c50293708af0aea3058
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-11 01:28:36 +0530

    classes, public and private

---
+++ clasess.ts
@@ -0,0 +1,11 @@
+class cUser{
+  public email : string =" we need to initilise this" // varaible 
+  private id: number = 34 //varaible
+  constructor( public gmail: string, public emailid: number){
+  }
+}
+const ali = new cUser("hi", 667);
+  
+
+export{}
+
