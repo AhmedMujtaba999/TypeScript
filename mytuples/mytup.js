ef23ddd81df2176aeb7122ff16d39db8701a1be7
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-10 21:55:53 +0530

    related js file

---
+++ mytuples/mytup.js
@@ -0,0 +1,9 @@
+//const user: (string| number| boolean)[]=[1, "hub", false] // order is not matter no strict sereis of type is neccseary
+let tUser; // tuple
+//tUser =[true, "hih", 34] // order matters so erros
+tUser = ["hi", 67, false]; // ordered of array with  string num bool
+let rgb = [355, 344, 456];
+const newUser = [22, "example@goolgle.com"];
+// wrong about tuple
+newUser[1] = "helloworld"; // manually u can chnage the value 
+export {};
