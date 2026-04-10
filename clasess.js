945f524f1db9299d780903a4bdffbeab48f22c73
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-11 01:29:00 +0530

    classes, public and private

---
+++ clasess.js
@@ -0,0 +1,12 @@
+"use strict";
+class cUser {
+    gmail;
+    emailid;
+    email = " we need to initilise this"; // varaible 
+    id = 34; //varaible
+    constructor(gmail, emailid) {
+        this.gmail = gmail;
+        this.emailid = emailid;
+    }
+}
+const ali = new cUser("hi", 667);
