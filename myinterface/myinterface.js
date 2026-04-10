c6b402fb88e5e98d46fe256bd8c3453566762124
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-11 00:52:07 +0530

    js file of interfaces ts file

---
+++ myinterface/myinterface.js
@@ -0,0 +1,13 @@
+const ahmed = {
+    dbid: 345,
+    email: "ahmed@.com",
+    userid: 123,
+    starttrail: () => {
+        return "trail started ";
+    },
+    getcoupon: (name, off) => {
+        return 20;
+    }
+};
+ahmed.email = "hihnh@.com";
+export {};
