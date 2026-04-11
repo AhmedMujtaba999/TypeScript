f554aaa00c6d4f79326a40dc8689181c213dd73b
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-11 23:51:41 +0530

    getter and setter

--- clasess.ts
+++ clasess.ts
@@ -1,4 +1,4 @@
-class cUser{
+class CUser{
 
     private _courseCount =1 
   public email : string =" we need to initilise this" // varaible 
@@ -6,9 +6,15 @@ class cUser{
   constructor( public gmail: string, public emailid: number){
   }
 
+  //private methods 
+
+  private deletetoken(){
+    console.log("Token deleted");
+  }
+
   //getter to get the value
    get getAppleEmail(): string{
-        return 'apple${this.email}'
+        return `apple${this.email}`
     }
   // getter to get the coursecount private
     get courseCount() : number {
@@ -17,15 +23,19 @@ class cUser{
 
     //setter to modify the value or update
 
-    set courseCount(courseNum) { // setter cannot have any datatype
+    set courseCount(courseNum : number) { // setter cannot have any datatype
       if(courseNum <=1){
         throw new Error("course count should be more than 1")
       }
       this._courseCount =courseNum
     }
 }
-const ali = new cUser("hi", 667);
+const ali = new CUser("hi", 667);
   
+
+let value =ali.courseCount //getter 
+ali.courseCount = 34; //setter
+console.log(ali.getAppleEmail);
+
  
 export{}
-
