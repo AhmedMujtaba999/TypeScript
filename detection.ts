891f80359a56c4921c8e32f0be39a36d53fbdb53
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-17 00:13:08 +0530

    narrowing in ts

---
+++ detection.ts
@@ -0,0 +1,27 @@
+function detecttype(val: number | string ){
+
+    if(typeof val === "string"){
+         return val.toLowerCase
+    }
+    return val+3
+}
+
+function provideId(id: string | null){
+    if(!id){
+        console.log("please provide id");
+    }
+    id?.toLowerCase()
+}
+
+
+function printAll(strs: string | string[] | null) {
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
