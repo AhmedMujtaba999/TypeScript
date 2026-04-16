9e00f16fc89dcd60b2a9459f85d4713bd2614130
ahmed <mujtabaaliahmed559@gmail.com>
2026-04-16 23:23:01 +0530

    generics in ts

---
+++ generics/mygenerics.ts
@@ -0,0 +1,29 @@
+const score: Array<number> =[]
+const names : Array<string> = []
+
+function identityOne(val: boolean| number): boolean | number {
+    return val
+}
+
+function identity(val: any): any{ // take number as input and return string so this can be possibel with any 
+    return val  
+}
+
+function identityTwo<T>( val : T): T{ // generics
+    return val
+}
+
+function identityThree<type>(val : type): type{ // take number (type) as input and return that type only 
+    return val
+}
+
+identityThree("hello") // string 
+identityThree(7) // number
+identityTwo(false) // boolean 
+
+
+interface boole{
+    brand: string,
+    type: number}
+
+    identityThree<boole>({brand: "hi", type: 78})// object paasing 
